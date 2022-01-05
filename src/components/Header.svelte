5c81ddf2c9929858714fb59316337ead77176c68
DarkDev <darkdev_brand@riseup.net>
2022-01-05 15:25:45 +0200

    add Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+</script>
+
+
+
+<TopAppBar variant="short">
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <!-- <Title>DarkDev Games</Title> -->
+    </Section>
+    <Section align="end" toolbar>
+      <!-- <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      > -->
+    </Section>
+  </Row>
+</TopAppBar>
