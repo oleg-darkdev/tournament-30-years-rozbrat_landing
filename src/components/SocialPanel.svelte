8cb6aacdf0ebc5a3eb4c6430923d82570cd71dca
DarkDev <darkdev_brand@riseup.net>
2022-01-07 02:02:16 +0200

    fix for mobile

--- src/components/SocialPanel.svelte
+++ src/components/SocialPanel.svelte
@@ -18,6 +18,10 @@
 		img: "img/social/fb_mini.png",
 		link: "",
 	},
+		{
+		img: "img/social/tweeter_mini.png",
+		link: "",
+	},
 			{
 		img: "img/social/patronite.png",
 		link: "",
@@ -30,10 +34,7 @@
 		img: "img/social/in_mini.png",
 		link: "",
 	},
-	{
-		img: "img/social/tweeter_mini.png",
-		link: "",
-	},
+
 		{
 		img: "img/social/etsy.png",
 		link: "",
@@ -47,11 +48,11 @@
 export let title = ''
 </script>
 
-  <Paper style="margin: 0 auto; height: auto; margin-bottom: 25px;" class="paper-demo">
+  <Paper style="margin: 0 auto; max-width: 500px; height: auto; margin-bottom: 25px;" class="paper-demo">
 	  <!--  👇 -->
     <Title style="font: 30px 'grafitty'; text-align: center;">{title + ` 💙`}</Title> 
     <!-- <Subtitle>This is a sheet of paper.</Subtitle> -->
-    <Content style="display: flex; flex-direction: row; justify-content: center; max-width: 500px; flex-wrap: wrap;">
+    <Content style="display: flex; flex-direction: row; justify-content: center; width: 100%; flex-wrap: wrap;">
 	{#each socialMini as social}
       <Card style="width: 70px; margin: 5px;">
 				<PrimaryAction on:click={() => location.href = social.link}>
