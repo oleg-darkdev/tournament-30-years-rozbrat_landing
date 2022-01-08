5843d65755057052d883d211db941f9719196548
DarkDev <darkdev_brand@riseup.net>
2022-01-08 17:52:21 +0200

    add table of content, refactoring: split for modules

---
+++ src/routes/handmade/AuctionsList.svelte
@@ -0,0 +1,85 @@
+<script>
+import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+} from '@smui/card';
+import Button, {
+    Group,
+    Label
+} from '@smui/button';
+import List, {
+    Item,
+    Text,
+    PrimaryText,
+    SecondaryText
+} from '@smui/list';
+import Chip, {
+    Set
+} from '@smui/chips';
+ import socialData from '../../data/socialMini';
+
+export let listOfAuctions;
+// console.log(listOfAuctions)
+</script>
+
+{#if !listOfAuctions.length}
+<div style="display: flex; justify-content: center;">
+    <Card style="max-width:450px; height: auto; ">
+        <Content>        
+            <p style="font: 28px 'grafitty'; max-width: 500px; color: #47babb; text-align: center;">At the moment I do not conduct auctions, but the patronite or store on etsy is always available 💙</p>
+        </Content>
+        <!-- <Actions> -->
+                   <div style="display: flex; width: 100%; min-width:350px; flex-wrap: wrap; flex-direction: column; height: auto; align-content: center;">
+
+        <Button style=" margin: 5px;" color="primary" variant="unelevated"  href={socialData.patronite.link} target="_blank">
+            <Label style="color: #fff;">🔥 Handmade on etsy </Label>
+        </Button>
+        <Button style="margin: 5px;" color="primary" variant="outlined"  href={socialData.patronite.link} target="_blank">
+            <Label style="color: #fff;">💸 Donate on patronite</Label>
+        </Button>
+        </div>
+        <!-- </Actions> -->
+    </Card>
+     </div>
+{:else}
+<List style="">
+    {#each listOfAuctions as auction}
+     <!-- background-image: linear-gradient(to top right, {auction.promo.brandColor} 0,9%, #191B1B 90%);  -->
+     <!--  -->
+    <Item style="display: flex; flex-direction: row; margin-bottom: 15px; border: 1px solid {auction.promo.brandColor};  flex-wrap: wrap;  height: auto; " on:click={() => location.href = `auctions/${auction.croundfanding.links.auction}`} >
+         <div style="display: flex; width: auto; margin-bottom: 5px;">
+               <Card style="max-height: 150px; width: 300px; margin: 5px 15px 20px 15px;">
+                <Media style="background-image: url({auction.promo.logo});" class="card-media-16x9" aspectRatio="16x9">
+                    <MediaContent>
+                    </MediaContent>
+                </Media>
+            </Card>
+        </div>
+             <div style="display: flex; margin-right: 15px; width: auto; flex-wrap: wrap; flex-direction: column; height: auto; justify-content: flex-start;">
+            <Text style="color: {auction.promo.brandColor};">
+            {auction.promo.shortName}
+
+        </Text>
+        <Set chips={[auction.lots.length]} let:chip nonInteractive>
+            <Chip {chip}>
+                <Text>{chip} lots in auction</Text>
+            </Chip>
+        </Set>
+        </div>
+       <div style="display: flex; width: 350px; flex-wrap: wrap; flex-direction: column; height: auto; align-content: flex-end;">
+        <Button style=" margin: 5px; background-color: {auction.promo.brandColor}; " variant="unelevated"  href={auction.croundfanding.links.allegro} target="_blank">
+            <Label style="color: #fff;">🔥 Auction on allegro </Label>
+        </Button>
+        <Button style="margin: 5px; background-color: {auction.promo.brandColor};" variant="outlined"  href={auction.croundfanding.links.patronitePl} target="_blank">
+            <Label style="color: #fff;">💸 Donate on patronite</Label>
+        </Button>
+        </div>
+    </Item>
+    {/each}
+</List>
+{/if}
---
+++ src/routes/handmade/TableOfContent.svelte
@@ -0,0 +1,72 @@
+<script>
+ import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+  } from '@smui/card';
+  import Button, { Label } from '@smui/button';
+  import IconButton, { Icon } from '@smui/icon-button';
+  import List, {
+    Item,
+    Text,
+    PrimaryText,
+    SecondaryText
+} from '@smui/list';
+
+export let arrayOfAuctions;
+export let activeAuctions;
+export let nonActiveAuctions;
+
+
+</script>
+
+<div style="display: flex; justify-content: center;align-items: center;   ">
+    <section style="width: 430px; margin: 0; display: flex;  height: auto; ">
+        <div class="tur-bg" style="flex-direction: column; align-items: center;">
+            <!-- max-width: 380px; -->
+            <Card style="width: 400px;  margin-bottom: 15px;">
+      <!-- <PrimaryAction on:click={() => clicked++}> -->
+        <Media  style="height: 515px; width: 100%; background-image: url('img/handmade.png');"/>
+        <Content class="mdc-typography--body2">
+              <p>              At the moment I am found money for the development and printing of my games, and i organized auctions of items that I made with my own hands.
+</p>
+          <!-- -->
+             <!-- В данный момент я собираю деньги на разработку и печать моих игр.   -->
+             <!-- At the moment I am found money for the development and printing of my games.
+              
+          
+        <Button href="#auctions" style="border-color: #47babb; margin: 5px;" variant="outlined">
+            <Label>Go to auctions</Label>
+        </Button> -->
+           <!-- <span style=" color: #47babb;">active auctions: {activeAuctions}</span> <br>
+           <span style=" color: #ff3e00;">paused auctions: {nonActiveAuctions}</span> <br> -->
+          <!-- <h3
+            class="mdc-typography--subtitle2"
+            style="margin: 0 0 10px; color: #888;"
+          >
+            And a subtitle.
+          </h3> -->
+        </Content>
+    </Card>
+
+
+    <Card  style="max-width: 400px;">
+      <Content component={List}>
+        <Item>
+          <Text style="color: #fff;">I raise money money for next games 👇</Text>
+        </Item>
+       
+        {#each arrayOfAuctions as game}
+          <Item on:click={() => location.href = `auctions/${game.croundfanding.links.auction}`}>
+            <Text style="color: #47babb">{game.promo.name}</Text>
+          </Item>
+        {/each}
+      </Content>
+    </Card>
+        </div>
+    </section>
+</div>
