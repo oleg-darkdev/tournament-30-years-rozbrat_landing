a10040f0d8155a8c3f569a84c4fda6011fb75317
DarkDev <darkdev_brand@riseup.net>
2022-01-08 17:51:21 +0200

    add import data for links, fix width for mobile

--- src/components/SocialPanel.svelte
+++ src/components/SocialPanel.svelte
@@ -1,49 +1,7 @@
 <script>
-		 import Card, {Content, PrimaryAction, Media} from '@smui/card';
-
-     const socialMini = [
-	{
-		img: "img/social/insta_mini.png",
-		link: "",
-	},
-	{
-		img: "img/social/youtube_mini.png",
-		link: "",
-	},
-		{
-		img: "img/social/telega_mini.png",
-		link: "",
-	},
-		{
-		img: "img/social/fb_mini.png",
-		link: "",
-	},
-		{
-		img: "img/social/tweeter_mini.png",
-		link: "",
-	},
-			{
-		img: "img/social/patronite.png",
-		link: "",
-	},
-	{
-		img: "img/social/pinterest.png",
-		link: "",
-	},
-	{
-		img: "img/social/in_mini.png",
-		link: "",
-	},
-
-		{
-		img: "img/social/etsy.png",
-		link: "",
-	},
-
-];
-    
-
-		 import Paper, { Title, Subtitle } from '@smui/paper';
+import Card, {Content, PrimaryAction, Media} from '@smui/card';
+import Paper, { Title, Subtitle } from '@smui/paper';
+import socialData from '../data/socialMini';
 
 export let title = ''
 </script>
@@ -52,8 +10,8 @@ export let title = ''
 	  <!--  👇 -->
     <Title style="font: 30px 'grafitty'; text-align: center;">{title + ` 💙`}</Title> 
     <!-- <Subtitle>This is a sheet of paper.</Subtitle> -->
-    <Content style="display: flex; flex-direction: row; justify-content: center; width: 100%; flex-wrap: wrap;">
-	{#each socialMini as social}
+    <Content style="display: flex; flex-direction: row; align-content: center; width: 100%; min-width: 300px; flex-wrap: wrap;">
+	{#each Object.values(socialData) as social}
       <Card style="width: 70px; margin: 5px;">
 				<PrimaryAction on:click={() => location.href = social.link}>
         <Media class="card-media-square" style="background-image: url('{social.img}');" aspectRatio="square">
