a70b889d7acd3dbf00808177dd748e13cdfc9ea1
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:15:14 +0200

    add Table of Content and navigation

---
+++ src/components/BtnTableOfContentStepsPanel/PreviousBtn.svelte
@@ -0,0 +1,15 @@
+<script>
+ import Card, {
+    Content,
+    PrimaryAction,
+  } from '@smui/card';
+
+export let step;
+</script>      
+
+
+<Card style='width: 150px; background-color: #47babb;  height: 80px; margin-bottom: 15px;'>
+    <PrimaryAction style="display: flex; align-items: center;" on:click={() => step--} padded>
+        <span style="font: 28px 'grafitty';">👈 Back</span>
+    </PrimaryAction>
+</Card>    
---
+++ src/components/faq/TableOfContent.svelte
@@ -0,0 +1,38 @@
+<script>
+ import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+  } from '@smui/card';
+import GetStarted from './TableOfContent/GetStarted.svelte';
+import AnchorList from './TableOfContent/AnchorList.svelte';
+
+
+export let step;
+</script>
+
+<div style="display: flex; justify-content: center;align-items: center;   ">
+    <section style="width: 430px; margin: 0; display: flex;  height: auto; ">
+        <div class="tur-bg" style="flex-direction: column; align-items: center; ">
+            <!-- max-width: 380px; -->
+            <Card style="width: 360px;  margin-bottom: 15px;">
+                  <Media  class={step == 0 ? '': 'hidden'} style="height: 460px; width: 100%; background-image: url('img/faq.png');"/>
+                  <Content class="mdc-typography--body2" style="display: flex; justify-content: center;align-items: center; flex-direction: column;">
+                      <GetStarted bind:step/>
+                      <AnchorList bind:step/>
+                  </Content>
+              </Card>
+        </div>
+    </section>
+</div>
+
+
+<style>
+  :global(.wrap-faq-hidden-block) {
+    display: flex; justify-content: center;align-items: center; flex-direction: column; 
+  }
+</style>
