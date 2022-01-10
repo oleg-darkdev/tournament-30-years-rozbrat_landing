7a12c3d1d13324b17ab78243d47e1e791843c3b9
DarkDev <darkdev_brand@riseup.net>
2022-01-10 16:00:31 +0200

    alpha version

---
+++ src/data/categoriesGames.js
@@ -0,0 +1,30 @@
+let categoriesGames = [
+	{
+		text: 'All',
+		emoji: ''
+	},
+	{
+		text: 'Historical',
+		emoji: '📜'
+	},
+	{
+		text: 'Art',
+		emoji: '🎨'
+	},
+	{
+		text: 'Social activism',
+		emoji: '👥'
+	},
+	{
+		text: 'It',
+		emoji: '🤖'
+	}
+	//         {
+	//     text: 'It',
+	//     emoji: ''
+	// },
+];
+
+
+
+export default categoriesGames;
