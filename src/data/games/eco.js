a6b501f377ef6b3d6d02634db59c0d6e3955cb7a
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:55:51 +0200

    add categories

--- src/data/games/eco.js
+++ src/data/games/eco.js
@@ -1,17 +1,19 @@
 import GameClass from './template'
-
+import categoriesGames from '../categoriesGames'
 
 let game = new GameClass();
 game.promo = {
 	name: '☘️ Eco bunt - eco activism',
 	shortName: '☘️ Eco',
-	banner: 'img/games/banners/eco_banner.png',
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/banners/eco_banner.png',
 	logo: 'img/games/logos/eco_logo.png',
 	status: 1, // 0 - alfa, 1 - beta, 2 - good,
 	shortDescription: '',
 	brandColor: '#339A37',
 	id: 'eco',
-	price: ''
+	price: '',
+	categories: [categoriesGames[1], categoriesGames[3]]
 };
 game.croundfanding = {
 	links: {
