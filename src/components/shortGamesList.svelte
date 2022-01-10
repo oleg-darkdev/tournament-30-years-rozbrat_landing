dbf25d8cd31a0cbb9de2184a973bebbc7f66e88f
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:54:42 +0200

    add short list view

---
+++ src/components/shortGamesList.svelte
@@ -0,0 +1,24 @@
+<script>
+  import List, {
+    Item,
+    Text
+} from '@smui/list';
+ import Card, {
+    Content
+  } from '@smui/card';
+
+  export let text;
+</script>   
+
+
+<Card  style="max-width: 400px;">
+    <Content component={List}>
+    <Item>
+        <Text style="color: #fff;">{text}</Text>
+    </Item>
+    <slot>
+
+
+    </slot>
+    </Content>
+</Card>
