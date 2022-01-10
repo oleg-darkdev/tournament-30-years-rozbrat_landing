e5f326c9505fc72a3fc4d151d8a3cc71a4674107
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:50:29 +0200

    add TOK for FAQ

--- src/components/faq/GameQuestionsList.svelte
+++ src/components/faq/GameQuestionsList.svelte
@@ -11,72 +11,39 @@ import Card, {
     Media,
     MediaContent,
 } from '@smui/card';
+import PartGameFaqList from './PartGameFaqList.svelte';
 
-import {
-    fade
-} from 'svelte/transition';
+
 
 export let data = {}
 </script>
 
 
- <Card style="max-width: 350px; width: auto;  background-color: {data.brandColor};">
-	<a id={data.id}></a>
-        <Media class="card-media-16x9" aspectRatio="16x9" style="background-image: url({data.logo}); width: 350px; height: 200"/>
+ <Card style="width: 100%; max-width: 100%; min-width: 300px;background-color: {data.promo.brandColor};">
+	<a id={data.promo.id}></a>
+        <Media style="background-image: url({data.promo.banner}); max-height: 300px; height: 230px; width: 100%; min-width: 300px; "/>
             <Content class="mdc-typography--body2">
-                <!-- <h2 class="mdc-typography--headline6" style="margin: 0;">
-            A card with media.
-                </h2> -->
-                <!-- <h3 style="color: #000; font: 36px 'grafitty'; margin: 0; color: #fff;">{data.name}</h3> -->
-				<Card style='width: 300px;  border-color: {data.brandColor};  height: 80px; margin-bottom: 15px;'>
-					<PrimaryAction style="display: flex; align-items: center;" on:click={() => {
-						location.href = `#${data.id}`
-						data.showFaq = !data.showFaq;
-					}} padded>
-						<span style="font: 35px 'grafitty'; color: #fff;">{data.name} FAQ </span>
-					</PrimaryAction>
-				</Card>
-
-				{#if data.showFaq}
-                <Group>
-                    <!-- other   -->
-                    <Subheader style="color: #000; font: 28px 'grafitty'; margin: 0; color: #fff;">Basic FAQ ⁉️</Subheader>
-                    <List style="max-width: 450px; width: 100%; height: auto;">
-                        {#each data.basic  as faq }
-                        <Item style="color: #fff;" on:click={() => faq.showAnswer = !faq.showAnswer}><Text style="color: #fff">{faq.question}</Text></Item>
-                        {#if faq.showAnswer}
-                        <div transition:fade class='answer'>
-                            <p class=" mdc-typography--body" >		{faq.answer}
-                            </p></div>
-                        {/if}
-                        {/each}
-                    </List>
-        <Subheader style="color: #000; font: 28px 'grafitty'; margin: 0; color: #fff;">Popular FAQ ⁉️</Subheader>
-                    <List style="max-width: 450px; width: 100%; height: auto;">
-                        {#each data.basic  as faq }
-                        <Item style="color: #fff;" on:click={() => faq.showAnswer = !faq.showAnswer}><Text style="color: #fff">{faq.question}</Text></Item>
-                        {#if faq.showAnswer}
-                        <div transition:fade class='answer'>
-                            <p class=" mdc-typography--body" >		{faq.answer}
-                            </p></div>
-                        {/if}
-                        {/each}
-                    </List>
-					        <Subheader style="color: #000; font: 28px 'grafitty'; margin: 0; color: #fff;">Other FAQ ⁉️</Subheader>
-                    <List style="max-width: 450px; width: 100%; height: auto;">
-                        {#each data.other  as faq }
-                        <Item style="color: #fff;" on:click={() => faq.showAnswer = !faq.showAnswer}><Text style="color: #fff">{faq.question}</Text></Item>
-                        {#if faq.showAnswer}
-                        <div transition:fade class='answer'>
-                            <p class=" mdc-typography--body" >		{faq.answer}
-                            </p></div>
-                        {/if}
-                        {/each}
-                    </List>
+                 <div style="display: flex; justify-content: center;">
+                    <Card style='width: 300px;  border-color: {data.promo.brandColor};  height: 80px; margin-bottom: 15px;'>
+                        <PrimaryAction style="display: flex; align-items: center;" on:click={() => {
+                            location.href = `#${data.promo.id}`
+                            data.showFaq = !data.showFaq;
+                        }} padded>
+                            <span style="font: 35px 'grafitty'; color: #fff;">{data.promo.shortName} FAQ </span>
+                        </PrimaryAction>
+                    </Card>
+                 </div>
+            {#if data.showFaq}
+            <div style="display: flex; justify-content: center; width: 100%;">
+                <Group style="width: 100%;">
+                    <PartGameFaqList text="Basic FAQ ⁉️" faqData={data.basicFAQ}/>
+                    <PartGameFaqList text="Popular FAQ ⁉️" faqData={data.popularFaq}/>
+                    <PartGameFaqList text="Other FAQ ⁉️" faqData={data.otherFaq}/>
                 </Group>
-				{/if}
-            </Content>
-            </Card>
+            </div>
+        {/if}
+    </Content>
+</Card>
 
 <style>
 
---
+++ src/components/faq/TableOfContent/AnchorList.svelte
@@ -0,0 +1,15 @@
+<script>
+import List, { Item, Separator, Text } from '@smui/list';
+import FAQCategories from '../../../data/faq/FAQCategories';
+export let step;
+</script>
+
+
+<div class={step == 1 ? '': 'hidden'}>
+    <List lass="demo-list">
+        {#each Object.values(FAQCategories) as category}
+        <!-- <a href='#{FAQCategories.shipping.id}' > -->
+            <Item style="width: 100%; font: 28px 'grafitty'; color: #fff;" ><Text>{category.text}</Text></Item>
+        {/each}
+    </List>
+</div>
