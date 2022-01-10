9d59de6a351b80712517fb3ff0f9deafce9a9295
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:52:23 +0200

    refactoring

---
+++ src/components/games/TableOfContent.svelte
@@ -0,0 +1,46 @@
+<script>
+ import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+  } from '@smui/card';
+
+
+
+import GetStarted from './GetStarted.svelte';
+import SelectView from './SelectView.svelte';
+import SelectCategory from './SelectCategory.svelte'
+import Result from './Result.svelte'
+
+export let step;
+
+export let options;
+</script>
+
+<div style="display: flex; justify-content: center;align-items: center;   ">
+    <section style="width: 430px; margin: 0; display: flex;  height: auto; ">
+        <div class="tur-bg" style="flex-direction: column; align-items: center; ">
+            <!-- max-width: 380px; -->
+            <Card style="width: 360px;  margin-bottom: 15px;">
+                  <Media  class={step == 0 ? '': 'hidden'} style="height: 400px; width: 100%; background-image: url('img/games.png');"/>
+                  <Content class="mdc-typography--body2" style="display: flex; justify-content: center;align-items: center; flex-direction: column;">
+                      <GetStarted bind:step/>
+                      <SelectView bind:step bind:options/>
+                      <SelectCategory bind:step bind:options/>
+                      <Result bind:step bind:options/>
+                  </Content>
+              </Card>
+        </div>
+    </section>
+</div>
+
+
+<style>
+  :global(.wrap-games-hidden-block) {
+    display: flex; justify-content: center;align-items: center; flex-direction: column; 
+  }
+</style>
