ded947da179eefd03cfdff34bcbbd22f1f7e67e2
DarkDev <darkdev_brand@riseup.net>
2022-01-10 16:01:07 +0200

    alpha v

---
+++ src/data/socialLinkReport.js
@@ -0,0 +1,35 @@
+const links = {
+    telegram: {
+        text: 'Telegram', 
+        link: '', 
+        img: ''
+    },
+    facebook: {
+        text: 'Facebook',
+        link: '',
+        img: ''
+    },
+    instagram: {
+        text: 'Instagram',
+        link: '',
+        img: ''
+    },
+    twitter: {
+        text: 'Twitter',
+        link: '',
+        img: ''
+    },
+    linkedin: {
+        text: 'Linkedin',
+        link: '',
+        img: ''
+    },
+    youtube: {
+        text: 'Youtube',
+        link: '',
+        img: ''
+    }
+};
+
+
+export default links;
