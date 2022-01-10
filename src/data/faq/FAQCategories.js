c56fb7596d428e913d8d286d876aded84d1f9485
DarkDev <darkdev_brand@riseup.net>
2022-01-10 15:47:29 +0200

    add FAQ data scheme

---
+++ src/data/faq/FAQCategories.js
@@ -0,0 +1,29 @@
+let FAQCategories = {
+	support: {
+		text: 'Support 💙',
+		id: 'support'
+	},
+	about: {
+		text: 'About me ℹ️',
+		id: 'about'
+	},
+	general: {
+		text: 'General Help ⁉️',
+		id: 'general'
+	},
+	workshops: {
+		text: 'Workshops 🧠',
+		id: 'workshops'
+	},
+	shipping: {
+		text: 'Shipping 🚚',
+		id: 'shipping'
+	},
+	typography: {
+		text: 'DarkDruk 📇',
+		id: 'typography'
+	}
+};
+
+
+export default FAQCategories;
