7f6cf33bbdd0fd355dab5dde6df375e3b3d28403
DarkDev <darkdev_brand@riseup.net>
2022-01-11 19:57:00 +0200

    refactoring main menu

---
+++ src/data/firstMenuText.js
@@ -0,0 +1,44 @@
+const firstMenu = [
+	// {
+	//   text: 'shop',
+	//   variable: shop,
+	//   link: 'shop',
+	//   emoji: '🛒'
+	// },
+	{
+		text: 'Boardgames',
+		link: 'games',
+		emoji: '🎲'
+	},
+
+	{
+		text: 'Workshops',
+		link: 'workshops',
+		emoji: '🧠'
+	},
+	{
+		text: 'Handmade',
+		link: 'handmade',
+		emoji: '🎨'
+	},
+	{
+		text: 'FAQs',
+		link: 'faq',
+		emoji: '⁉️'
+	},
+
+	{
+		text: 'Support ME',
+		link: 'support',
+		emoji: '💙'
+	}
+
+	// {
+	//   text: 'about',
+	//   link: 'about',
+	//   emoji: 'ℹ️'
+	// }
+];
+
+
+export default firstMenu;
