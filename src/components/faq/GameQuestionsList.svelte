11debf07d2b4bafa65ae119e3570af60fb9b1e22
DarkDev <darkdev_brand@riseup.net>
2022-01-11 19:57:38 +0200

    upd margin

--- src/components/faq/GameQuestionsList.svelte
+++ src/components/faq/GameQuestionsList.svelte
@@ -19,20 +19,21 @@ export let data = {}
 </script>
 
 
- <Card style="width: 100%; max-width: 100%; min-width: 300px;background-color: {data.promo.brandColor};">
+ <Card style="width: 100%; max-width: 100%; min-width: 300px;background-color: {data.promo.brandColor}; margin-bottom: 15px;">
 	<a id={data.promo.id}></a>
         <Media style="background-image: url({data.promo.banner}); max-height: 300px; height: 230px; width: 100%; min-width: 300px; "/>
             <Content class="mdc-typography--body2">
-                 <div style="display: flex; justify-content: center;">
-                    <Card style='width: 300px;  border-color: {data.promo.brandColor};  height: 80px; margin-bottom: 15px;'>
+                <div style="display: flex; justify-content: center;">
+                    <Card style='min-width: 320px; width: auto; border-color: {data.promo.brandColor};  height: 80px; margin-bottom: 15px;'>
                         <PrimaryAction style="display: flex; align-items: center;" on:click={() => {
                             location.href = `#${data.promo.id}`
                             data.showFaq = !data.showFaq;
                         }} padded>
-                            <span style="font: 35px 'grafitty'; color: #fff;">{data.promo.shortName} FAQ </span>
+                            <span style="font: 35px 'grafitty'; color: #fff;">{data.promo.shortName} - FAQ</span>
                         </PrimaryAction>
                     </Card>
                  </div>
+
             {#if data.showFaq}
             <div style="display: flex; justify-content: center; width: 100%;">
                 <Group style="width: 100%;">
