1c1a753f0acacb0e6200c1965556c617851debda
DarkDev <darkdev_brand@riseup.net>
2022-01-11 19:51:40 +0200

    create FAQ for all games - alpha

---
+++ src/data/faq/games/graffity.js
@@ -0,0 +1,15 @@
+// import basicFAQ from './gulagUSSR/basicQuestions';
+// import otherFaq from './gulagUSSR/otherFaq';
+// import popularFaq from './gulagUSSR/popularFaq';
+// import gameData from '../../gamesData';
+// import FAQ from './template';
+
+// let gameFAQ = new FAQ(
+// 	gameData.graffity.promo,
+// 	gameData.graffity.croundfanding,
+// 	basicFAQ,
+// 	popularFaq,
+// 	otherFaq
+// );
+
+// export default gameFAQ;
--- src/data/faq/questionsGames.js
+++ src/data/faq/questionsGames.js
@@ -1,6 +1,6 @@
 import gulagUSSR from './games/gulagUSSR';
-// import eco from './games/eco';
-// import fnb from './games/fnb';
+import eco from './games/eco';
+import fnb from './games/fnb';
 
 // import graffity from './games/graffity';
 // import gulagRussia from './games/gulagRussia';
@@ -8,22 +8,22 @@ import gulagUSSR from './games/gulagUSSR';
 // import linux from './games/linux';
 // import kanbanel from './games/kanbanel';
 // import ksg from './games/ksg';
-// import a from './games/aGame';
-// import hackerspace from './games/hackerspace';
-// import katyn from './games/katyn';
+import a from './games/aGame';
+import hackerspace from './games/hackerspace';
+import katyn from './games/katyn';
 
-// import lgbt from './games/lgbt';
-// import ror from './games/ror';
-// import squat from './games/squat';
-// import wikileaks from './games/wikileaks';
-// import wtb from './games/wtb';
-// import yakusa from './games/yakusa';
+import lgbt from './games/lgbt';
+import ror from './games/ror';
+import squat from './games/squat';
+import wikileaks from './games/wikileaks';
+import wtb from './games/wtb';
+import yakusa from './games/yakusa';
 
 const gamesFaq = {
 	gulagUSSR: gulagUSSR,
-	// a: a,
-	// eco: eco,
-	// fnb: fnb,
+	a: a,
+	eco: eco,
+	fnb: fnb,
 
 	// linux,
 	// tattoo,
@@ -32,13 +32,13 @@ const gamesFaq = {
 	// kanbanel,
 	// ksg,
 
-	// hackerspace: hackerspace,
-	// katyn: katyn,
-	// lgbt: lgbt,
-	// ror: ror,
-	// squat: squat,
-	// wikileaks: wikileaks,
-	// wtb: wtb,
-	// yakusa: yakusa
+	hackerspace: hackerspace,
+	katyn: katyn,
+	lgbt: lgbt,
+	ror: ror,
+	squat: squat,
+	wikileaks: wikileaks,
+	wtb: wtb,
+	yakusa: yakusa
 };
 export default gamesFaq;
