0837fbd9ac243e8d3cb4f7fafee579bf4ea2b63a
DarkDev <darkdev_brand@riseup.net>
2022-01-17 22:19:55 +0200

    add cross browser fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -86,13 +86,16 @@ function setActive(value) {
 <Footer />
 
 <style>
-    @font-face {
-        font-family: 'grafitty';
-        font-style: normal;
-        font-weight: 400;
-        src: local('grafitty'), format('otf');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-    }
+  @font-face {
+    font-family: 'grafitty';
+    src: url('grafitty-webfont.woff2') format('woff2'),
+        url('grafitty-webfont.otf') format('otf'),
+         url('grafitty-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+/* src: local('grafitty'), format('otf'); */
+}
+
   :global(.bg-img) {
  background-position: center center;      
     background-repeat: no-repeat;      
