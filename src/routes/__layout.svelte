52961693fa8918070e7589d2531a53cd345d065d
DarkDev <darkdev_brand@riseup.net>
2022-01-17 23:18:35 +0200

    fix font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -88,12 +88,12 @@ function setActive(value) {
 <style>
   @font-face {
     font-family: 'grafitty';
-    src: url('https://github.com/oleg-darkdev/dd/blob/deploy/static/grafitty-webfont.woff2') format('woff2'),
+    /* src: url('https://github.com/oleg-darkdev/dd/blob/deploy/static/grafitty-webfont.woff2') format('woff2'),
         url('https://github.com/oleg-darkdev/dd/blob/deploy/static/grafitty-webfont.otf') format('otf'),
-         url('https://github.com/oleg-darkdev/dd/blob/deploy/static/grafitty-webfont.woff') format('woff');
+         url('https://github.com/oleg-darkdev/dd/blob/deploy/static/grafitty-webfont.woff') format('woff'); */
     font-weight: normal;
     font-style: normal;
-/* src: local('grafitty'), format('otf'); */
+    src: local('grafitty'), format('otf');
 }
 
   :global(.bg-img) {
