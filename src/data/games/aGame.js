5e8c8eba0ee90b148a8e6d5ee2f3b0d4cc6d8bcc
DarkDev <darkdev_brand@riseup.net>
2022-01-17 15:43:02 +0200

    add roles images

--- src/data/games/aGame.js
+++ src/data/games/aGame.js
@@ -3,9 +3,10 @@ import categoriesGames from '../categoriesGames';
 
 let game = new GameClass();
 game.promo = {
-	name: 'Theory of Anarchism', // 🏴
-	shortName: 'Anarchism',
-	banner: 'img/games/banners/A_banner.png',
+	name: '🏴 Theory of Anarchism', //
+	shortName: '🏴 Anarchism',
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/banners/A_banner.png',
 	logo: 'img/games/logos/A_logo.png',
 	status: 1, // 0 - alfa, 1 - beta, 2 - good,
 	shortDescription: '',
@@ -13,6 +14,42 @@ game.promo = {
 	id: 'A',
 	price: '',
 	categories: [categoriesGames[1], categoriesGames[2], categoriesGames[5], categoriesGames[6]]
+};
+game.moreInfo = {
+	players: '2-8',
+	age: '16+',
+	time: '120-180',
+	LOR: [
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	],
+	lorImg: 'img/games/a/banners/persons_banner.png',
+	goal: '',
+	educationMoments: ['', ''],
+	description: [
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	],
+	targetAudience: '',
+	photos: ['', '', ''],
+	personsImg: 'img/games/a/persons/agorist_person.png',
+	roles: [
+		{
+			name: 'Lorem ipsum',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'img/games/a/persons/agorist_person.png'
+		},
+		{
+			name: 'Lorem ipsum',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'img/games/a/persons/capitalist_person.png'
+		}
+	]
 };
 game.croundfanding = {
 	links: {
