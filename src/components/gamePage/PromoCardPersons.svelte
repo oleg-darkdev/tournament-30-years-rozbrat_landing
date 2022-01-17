0eb5bdefb10c97dd4c21738aa4b180a1b8ba1e7a
DarkDev <darkdev_brand@riseup.net>
2022-01-17 22:05:52 +0200

    upd

---
+++ src/components/gamePage/PromoCardPersons.svelte
@@ -0,0 +1,35 @@
+<script>
+import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+} from '@smui/card';
+import Button, {
+    Label
+} from '@smui/button';
+import IconButton, {
+    Icon
+} from '@smui/icon-button';
+
+
+export let moreInfo;
+export let promo;
+</script>
+
+<Card style="margin-bottom: 15px; width: 100%; max-width: auto; background-color: {promo.brandColor};">
+<Media  style="height: 250px; background-image: url({moreInfo.roles.banner});"/>
+    <Content class="mdc-typography--body2" style="color: #191B1B;">
+        <h2 class="mdc-typography--headline6" style="margin: 0;">
+            Characters available in the game
+        </h2>
+        {#each moreInfo.roles.description as description}
+            <p>{description}</p>        
+        {/each}
+    </Content>
+</Card>
+
+
