c651dfe920dc53b027d0975384fa08effa27d9c3
DarkDev <darkdev_brand@riseup.net>
2022-01-30 18:47:23 +0200

    fix

--- src/data/workshops/ksg.js
+++ src/data/workshops/ksg.js
@@ -1,26 +1,26 @@
-// import gamesData from '../gamesData';
-// import Workshop from './template';
+import gamesData from '../gamesData';
+import Workshop from './template';
 
-// let gameData = gamesData.gulagUSSR;
+let gameData = gamesData.ksg;
 
-// let historyWorkshops = [
-// 	{
-// 		date: '22.02.2022',
-// 		place: 'ul. Puławska 37, Warsawa',
-// 		reports: [
-// 			'',
-// 			'',
-// 			'',
-// 			'',
-// 			'',
-// 			''
-// 			// telegram facebook instagram twitter linkedin youtube
-// 		],
-// 		maxPeople: 6,
-// 		eventbrite: 'https://www.eventbrite.com/'
-// 	}
-// ];
-// let workshop = new Workshop(gameData.promo, gameData.croundfanding, gameData.moreInfo);
-// workshop.history = historyWorkshops;
+let historyWorkshops = [
+	{
+		date: '22.02.2022',
+		place: 'ul. Puławska 37, Warsawa',
+		reports: [
+			'',
+			'',
+			'',
+			'',
+			'',
+			''
+			// telegram facebook instagram twitter linkedin youtube
+		],
+		maxPeople: 6,
+		eventbrite: 'https://www.eventbrite.com/'
+	}
+];
+let workshop = new Workshop(gameData.promo, gameData.croundfanding, gameData.moreInfo);
+workshop.history = historyWorkshops;
 
-// export default workshop;
+export default workshop;
