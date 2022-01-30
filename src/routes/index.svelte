a65a7ace17005f7d09ecb04567bf00c050284bef
DarkDev <darkdev_brand@riseup.net>
2022-01-30 13:51:04 +0200

    refactoring bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 import { onMount } from 'svelte';
 import AOS from 'aos';
 
-import Menu from '../components/landingPage/mainMenu.svelte';
+import Menu from '../components/landingPage/MainMenu.svelte';
 import GameMenu from '../components/landingPage/GameMenu.svelte';
 import AnimationLeftWrap from '../layout/AnimationLeftWrap.svelte';
 import AnimationRightWrap from '../layout/AnimationRightWrap.svelte';
@@ -19,33 +19,36 @@ onMount(() => {
 <svelte:head>
     <title>DarkDev Games</title>
 </svelte:head>
-<div class="wrap-game-block" style="height: 300px; z-index: 0; margin-top: 0px; background-image: url('img/bg/first_bg.png');">
-</div>      
-<div class="index-menu" style="position: absolute; top: 20px; left: 37%; z-index: 1; ">
-    <section style="width: 430px; margin: 0; ">
-        <div class="tur-bg" >
-            <Menu />
-        </div>
-    </section>
-</div>
+<div class="wrap-game-block" style="height: 250px; z-index: 0; margin-top: 0px; background-image: url('img/bg/first_bg.png');">
+</div>   
+<div class="wrap-game-block" style="min-height: 200px;  background-color: #47babb;">
+    <div class="index-menu" style="position: absolute; top: 20px; left: 37%; z-index: 1; ">
+        <section style="width: 430px; margin: 0; ">
+            <div class="tur-bg" >
+                <Menu />
+            </div>
+        </section>
+    </div>
+</div>    
 
-<AnimationRightWrap backgroundImg='img/covid_bg.png'>
+
+<AnimationRightWrap backgroundImg='https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/covid_bg.png'>
     <GameMenu gameData={gamesData.covid}/>
 </AnimationRightWrap>
 
-<AnimationLeftWrap backgroundImg='img/a_bg.png'>
+<AnimationLeftWrap backgroundImg='https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/a_bg.png'>
     <GameMenu gameData={gamesData.a}/>
 </AnimationLeftWrap>
 
-<AnimationRightWrap backgroundImg='img/graffity_bg.png'>
+<AnimationRightWrap backgroundImg='https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/graffity_bg.png'>
     <GameMenu gameData={gamesData.graffity}/>
 </AnimationRightWrap>
 
- <AnimationLeftWrap backgroundImg='img/linux_bg.png'>
+ <AnimationLeftWrap backgroundImg='https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/linux_bg.png'>
     <GameMenu gameData={gamesData.linux}/>
 </AnimationLeftWrap>
 
-<AnimationRightWrap backgroundImg='img/ksg_bg.png'>
+<AnimationRightWrap backgroundImg='https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/ksg_bg.png'>
     <GameMenu gameData={gamesData.ksg}/>
 </AnimationRightWrap>
 
