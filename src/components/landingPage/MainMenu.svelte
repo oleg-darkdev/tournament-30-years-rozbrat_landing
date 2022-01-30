b8538e2e6fb4c6e77fabdc6d01a892069da4c1a5
DarkDev <darkdev_brand@riseup.net>
2022-01-30 16:53:31 +0200

    refactoring

---
+++ src/components/landingPage/MainMenu.svelte
@@ -0,0 +1,43 @@
+<script>
+import MenuBtn from './MenuBtn.svelte'
+import firstMenuText from '../../data/firstMenuText';
+
+import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+} from '@smui/card';
+
+let getStarted;
+</script> 
+
+
+
+
+<Card style='width: 380px; margin: 0; height: auto; background-color: #000000;'>
+    {#if !getStarted}
+    <Media  style='background-image: url("img/glad_to_see_you.png");  height: 390px; width: 100%;' ></Media>
+    {/if}
+    <Content class="mdc-typography--body2" style="display: flex;
+        align-items: center; background-color: #050506; flex-direction: column;
+        ">
+        {#if !getStarted}
+        <Card style='width: 250px; background-color: #47babb;  height: 80px; margin-bottom: 15px;'>
+            <PrimaryAction style="display: flex; align-items: center;" on:click={() => getStarted = !getStarted} padded>
+                <span style="font: 35px 'grafitty';">👉 MENU</span>
+            </PrimaryAction>
+        </Card>
+
+        {:else}
+            {#each firstMenuText as menuItem}
+                <MenuBtn color="#47babb">                        
+                    <a href={menuItem.link}><span style="font: 40px 'grafitty';">{menuItem.text} {menuItem.emoji}</span> </a>
+                </MenuBtn>
+            {/each}
+        {/if}
+    </Content>
+</Card>
