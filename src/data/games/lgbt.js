fc253c93a3307f576e621dc051147c00cbc7e879
DarkDev <darkdev_brand@riseup.net>
2022-02-01 21:13:04 +0200

    add images banners and characters

--- src/data/games/lgbt.js
+++ src/data/games/lgbt.js
@@ -7,7 +7,10 @@ let game = new GameClass();
 game.promo = {
 	name: '🌈 LGBT+',
 	shortName: '🌈 LGBT+',
-	banner: 'img/games/banners/lgbt_banner.png',
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/banners/lgbt_banner.png',
+	landingBanner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/d45fec7333c68c75264acfead3be04a4de16b2bf/static/img/bg/lgbt_bg.png',
 	logo: 'img/games/logos/lgbt_logo.png',
 	status: 1, // 0 - alfa, 1 - beta, 2 - good,
 	shortDescription: '',
@@ -23,5 +26,87 @@ game.croundfanding = {
 	},
 	goal: ''
 };
+game.roles = {
+	description: [
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	],
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/banners/persons_banner.png',
+	characters: [
+		{
+			name: 'Achillean',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/achillean_person.png'
+		},
+		{
+			name: 'Aromantic',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/aromantic_person.png'
+		},
+		{
+			name: 'Bigender',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/bigender_person.png'
+		},
+		{
+			name: 'Bisexual',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/bisexual_person.png'
+		},
+		{
+			name: 'Diamoric',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/diamoric_person.png'
+		},
+		{
+			name: 'Libra masculine',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/libra_masculine_person.png'
+		},
+		{
+			name: 'Libra feminine',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/librafeminine_person.png'
+		},
+		{
+			name: '',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: ''
+		},
+		{
+			name: 'Mavierique',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/mavierique_person.png'
+		},
+		{
+			name: 'Torik',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/torik_person.png'
+		},
+		{
+			name: 'Trixic',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/trixic_person.png'
+		},
+		{
+			name: 'Trigender',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/lgbt/persons/trigender_person.png'
+		}]
+};
+
 
 export default game;
--- src/data/games/squat.js
+++ src/data/games/squat.js
@@ -7,7 +7,10 @@ let game = new GameClass();
 game.promo = {
 	name: '🏠 Squat action',
 	shortName: '🏠 Squat action',
-	banner: 'img/games/banners/squat_banner.png',
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/banners/squat_banner.png',
+	landingBanner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/bg/squat_bg.png',
 	logo: 'img/games/logos/squat_logo.png',
 	status: 1, // 0 - alfa, 1 - beta, 2 - good,
 	shortDescription: '',
@@ -23,5 +26,74 @@ game.croundfanding = {
 	},
 	goal: ''
 };
-
+game.roles = {
+	description: [
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	],
+	banner:
+		'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/banners/persons_banner.png', // new
+	characters: [
+		{
+			name: 'Cops',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/acab_person.png'
+		},
+		{
+			name: 'White trash',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/whitetrash_person.png'
+		},
+		{
+			name: 'Capitalist',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/capitalist_person.png'
+		},
+		{
+			name: 'A.D.A. squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/ada_person.png'
+		},
+		{
+			name: 'ELBA squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/elba_person.png'
+		},
+		{
+			name: 'Hulai pole squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/hulaipole_person.png'
+		},
+		{
+			name: 'Pandamia squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/pandemia_person.png'
+		},
+		{
+			name: 'Przychodnia squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/przychodnia_person.png'
+		},
+		{
+			name: 'Syrena squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/syrena_person.png'
+		},
+		{
+			name: 'Rozbrat squat',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/squat/persons/rozbrat_person.png'
+		}
+	]
+};
 export default game;
