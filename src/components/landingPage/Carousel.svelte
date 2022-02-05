16b876b748a5a285df7aadfb97a3d0bbf9a6d97e
DarkDev <darkdev_brand@riseup.net>
2022-02-05 12:52:39 +0200

    add carousel for games promo images

---
+++ src/components/landingPage/Carousel.svelte
@@ -0,0 +1,142 @@
+<script>
+  import { flip } from 'svelte/animate';
+  import { onDestroy } from 'svelte';
+
+  export let images;
+//   export let imageWidth = 300;
+//   export let imageSpacing = 20;
+  export let speed = 2000;
+  export let controlColor= '#444';
+  export let controlScale = '0.5';
+  export let autoplay = false;
+  export let autoplaySpeed = 5000;
+  let interval;
+
+
+  const rotateLeft = e => {
+    const transitioningImage = images[images.length - 1]
+    document.getElementById(transitioningImage.id).style.opacity = 0;
+    images = [images[images.length -1],...images.slice(0, images.length - 1)]
+    setTimeout(() => (document.getElementById(transitioningImage.id).style.opacity = 1), speed);
+  }
+
+  const rotateRight = e => {
+    const transitioningImage = images[0]
+    document.getElementById(transitioningImage.id).style.opacity = 0;
+    images = [...images.slice(1, images.length), images[0]]
+    setTimeout(() => (document.getElementById(transitioningImage.id).style.opacity = 1), speed);
+  }
+
+  const startAutoPlay = () => {
+    if(autoplay){
+      interval = setInterval(rotateLeft, autoplaySpeed)
+    }
+  }
+
+  const stopAutoPlay = () => {
+    clearInterval(interval)
+  }
+
+  if(autoplay){
+    startAutoPlay()
+  }
+
+  onDestroy(()=>{stopAutoPlay()})
+
+</script>
+
+<div id="carousel-container">
+  <div id="carousel-images">
+    {#each images as image (image.id)}
+    <div style=''
+        on:mouseover={stopAutoPlay}
+        on:mouseout={startAutoPlay}
+        animate:flip={{duration: speed}}>
+        <img style='width: 350px; margin: 0 15px; height: 350px; border-radius: 2%;'
+            src={image.path}
+            alt={image.id}
+            id={image.id}
+        />
+    </div>
+    {/each}
+  </div>
+  <button id="left" on:click={rotateLeft}>
+    <slot name="left-control">
+        <svg width="39px" height="110px" id="svg8" transform={`scale(${controlScale})`}>
+            <g id="layer1" transform="translate(-65.605611,-95.36949)">
+            <path
+            style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+            d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+            id="path1412" />
+            </g>
+        </svg>
+    </slot>
+  </button>
+  <button id="right" on:click={rotateRight}>
+    <slot name="right-control">
+      <svg width="39px" height="110px" id="svg8" transform={`rotate(180) scale(${controlScale})`}>
+        <g id="layer1" transform="translate(-65.605611,-95.36949)">
+          <path
+          style={`fill:none;stroke:${controlColor};stroke-width:9.865;stroke-linecap:round;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`}
+          d="m 99.785711,100.30199 -23.346628,37.07648 c -7.853858,12.81098 -7.88205,12.81098 0,24.78902 l 23.346628,37.94647"
+          id="path1412" />
+        </g>
+      </svg>
+    </slot>
+  </button>
+
+</div>
+
+<style>
+  #carousel-container {
+    width: 100%;
+    /* margin-right: -100px; */
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    overflow-x: hidden;
+  }
+  #carousel-images {
+    display: flex;
+    justify-content: center;
+    flex-wrap: nowrap;
+    -webkit-mask: linear-gradient(
+      to right,
+      transparent,
+      black 40%,
+      black 60%,
+      transparent
+    );
+    mask: linear-gradient(
+      to right,
+      transparent,
+      black 40%,
+      black 60%,
+      transparent
+    );
+  }
+
+  button {
+   position: absolute;
+   top: 50%;
+   transform: translateY(-50%);
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   background: transparent;
+   border: none;
+ }
+
+ button:focus {
+   outline: auto;
+ }
+
+  #left {
+    left: 10px;
+  }
+
+  #right {
+    right: 10px;
+  }
+
+</style>
--- src/components/landingPage/MainMenu.svelte
+++ src/components/landingPage/MainMenu.svelte
@@ -18,14 +18,13 @@ let getStarted;
 
 
 
-<Card style='width: 380px; margin: 0; height: auto; background-color: #000000;'>
-    {#if !getStarted}
+<Card style='min-width: 380px;  max-width:700px;  margin: 0; height: auto; background-color: #000000;'>
+    <!-- {#if !getStarted}
     <Media  style='background-image: url("img/glad_to_see_you.png");  height: 390px; width: 100%;' ></Media>
-    {/if}
+    {/if} -->
     <Content class="mdc-typography--body2" style="display: flex;
-        align-items: center; background-color: #050506; flex-direction: column;
-        ">
-        {#if !getStarted}
+        align-items: center; background-color: #050506; flex-direction: row; flex-wrap: wrap; justify-content: center;">
+        <!-- {#if !getStarted}
         <Card style='width: 250px; background-color: #47babb;  height: 80px; margin-bottom: 15px;'>
             <PrimaryAction style="display: flex; align-items: center;" on:click={() => getStarted = !getStarted} padded>
                 <span style="font: 35px 'grafitty';">👉 MENU</span>
@@ -33,11 +32,18 @@ let getStarted;
         </Card>
 
         {:else}
-            {#each firstMenuText as menuItem}
-                <MenuBtn color="#47babb">                        
-                    <a href={menuItem.link}><span style="font: 40px 'grafitty';">{menuItem.text} {menuItem.emoji}</span> </a>
-                </MenuBtn>
+             -->
+             {#each firstMenuText as menuItem}
+                <a href={menuItem.link}><MenuBtn bg={menuItem.img}>                        
+                    <!-- {menuItem.emoji} {menuItem.text} -->
+                    
+                </MenuBtn></a>
             {/each}
-        {/if}
+            <Card style='width: 100%; max-width: 760px; width; auto;    background-position: center; background-repeat: no-repeat; background-size: cover; background-image: url(img/menuBtn/support_btn.png); border: 2px solid #191B1B; height: 90px; margin: 6px;  '>
+                <a href='support'><PrimaryAction style="display: flex; align-items: center; " padded>                    
+                   
+                   </PrimaryAction> </a>
+            </Card>
+        <!-- {/if} -->
     </Content>
 </Card>
