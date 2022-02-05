b6d8cc98311ef6424cc49429982654d913066183
DarkDev <darkdev_brand@riseup.net>
2022-02-05 12:53:04 +0200

    refactoring menu btn`s

--- src/data/firstMenuText.js
+++ src/data/firstMenuText.js
@@ -1,43 +1,73 @@
 const firstMenu = [
 	// {
-	//   text: 'shop',
-	//   variable: shop,
-	//   link: 'shop',
-	//   emoji: '🛒'
+	// 	text: 'Boardgames',
+	// 	link: 'games',
+	// 	emoji: '🎲'
 	// },
+
+	// {
+	// 	text: 'Workshops',
+	// 	link: 'workshops',
+	// 	emoji: '🧠'
+	// },
+	// {
+	// 	text: 'Handmade',
+	// 	link: 'handmade',
+	// 	emoji: '🎨'
+	// },
+	// {
+	// 	text: 'About',
+	// 	link: 'about',
+	// 	emoji: 'ℹ️'
+	// },
+	// {
+	// 	text: 'FAQs',
+	// 	link: 'faq',
+	// 	emoji: '⁉️'
+	// },
+
+	// {
+	// 	text: 'Support ME',
+	// 	link: 'support',
+	// 	emoji: '💙'
+	// },
+
 	{
 		text: 'Boardgames',
 		link: 'games',
-		emoji: '🎲'
+		emoji: '🎲',
+		img: 'img/menuBtn/bgg_btn.png'
 	},
 
 	{
 		text: 'Workshops',
 		link: 'workshops',
-		emoji: '🧠'
+		emoji: '🧠',
+		img: 'img/menuBtn/workshops_btn.png'
 	},
+	// {
+	// 	text: 'Handmade',
+	// 	link: 'handmade',
+	// 	emoji: '🎨'
+	// },
 	{
-		text: 'Handmade',
-		link: 'handmade',
-		emoji: '🎨'
-	},
-	{
-	  text: 'About',
-	  link: 'about',
-	  emoji: 'ℹ️'
+		text: 'About',
+		link: 'about',
+		emoji: 'ℹ️',
+		img: 'img/menuBtn/about_btn.png'
 	},
 	{
 		text: 'FAQs',
 		link: 'faq',
-		emoji: '⁉️'
-	},
+		emoji: '⁉️',
+		img: 'img/menuBtn/faq_btn.png'
+	}
 
-	{
-		text: 'Support ME',
-		link: 'support',
-		emoji: '💙'
-	},
-
+	// {
+	// 	text: 'Support ME',
+	// 	link: 'support',
+	// 	emoji: '💙'
+	// }
 ];
 
 
