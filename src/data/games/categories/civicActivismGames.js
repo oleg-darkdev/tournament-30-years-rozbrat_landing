012dfddc613bd1d1f1587148d7d01f528b49e7db
DarkDev <darkdev_brand@riseup.net>
2022-02-21 14:44:04 +0200

    add samba ROR, fnb

--- src/data/games/categories/civicActivismGames.js
+++ src/data/games/categories/civicActivismGames.js
@@ -1,19 +1,19 @@
 import a from '../aGame';
-// import fnb from '../fnb';
+import fnb from '../fnb';
 // import eco from '../eco';
 import squat from '../squat';
-// import ror from '../ror';
+import ror from '../ror';
 import lgbt from '../lgbt';
 // import hackerspace from '../hackerspace';
 
 const games = {
 	a,
-	// fnb,
+	fnb,
 	// eco,
 	squat,
 	// hackerspace,
 	lgbt,
-	// ror,
+	ror,
 };
 
 export default games;
