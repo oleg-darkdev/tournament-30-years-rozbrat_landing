842089605a3d73fbb758442f6f0cbe3e59bd113a
DarkDev <darkdev_brand@riseup.net>
2022-02-22 13:42:33 +0200

    add colors, upd order

--- src/data/games/ksg.js
+++ src/data/games/ksg.js
@@ -27,93 +27,106 @@ game.roles = {
 	banner: '',
 	characters: [
 		{
-			name: 'Chappie',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/chappie_person.png'
-		},
-		{
-			name: 'Satan',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/satan_person.png'
-		},
-		,
-		{
-			name: 'Tedy',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/tedy_person.png'
-		},
-		{
 			name: 'Thunderbird',
+			bgColor: '#809b29',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/thunderbird_person.png'
 		},
 		{
 			name: 'Vagosh',
+			bgColor: '#70794f',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/vagosh_person.png'
 		},
 		{
+			name: 'Satan',
+			bgColor: '#e30613',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/satan_person.png'
+		},
+		{
 			name: 'Yoki',
+			bgColor: '#b33209',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/yoki_person.png'
 		},
 		{
+			name: 'Tedy',
+			bgColor: '#f7b6a4',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/tedy_person.png'
+		},
+		{
 			name: 'Prospector',
+			bgColor: '#fed89c',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/prospector_person.png'
 		},
 		{
 			name: 'Geotecton',
+			bgColor: '#b9cf31',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/geotecton_person.png'
 		},
 		{
 			name: 'Geognosia',
+			bgColor: '#fcbe00',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/geognosia_person.png'
 		},
 		{
 			name: 'Earthlinger',
+			bgColor: '#37464f',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/earthlinger_person.png'
 		},
-
+		{
+			name: 'Chappie',
+			bgColor: '#97501e',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/chappie_person.png'
+		},
 		{
 			name: 'Xeno',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/xeno_person.png'
 		},
 		{
 			name: 'Polygenro',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/polygenro_person.png'
 		},
 		{
-			name: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/khaetano_person.png',
+			name: 'Khaetano',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			img: ''
+			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/khaetano_person.png'
 		},
 		{
 			name: 'Isfeno',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/isfeno_person.png'
 		},
 		{
 			name: 'Kato',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/kato_person.png'
@@ -121,6 +134,8 @@ game.roles = {
 
 		{
 			name: 'Fevagno',
+			bgColor: '#e6007e',
+			bgColor: '#000000',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 			img: 'https://raw.githubusercontent.com/oleg-darkdev/dd/deploy/static/img/games/ksg/persons/fevagno_person.png'
