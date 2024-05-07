c9408083d50cee56db02d0fdf31d8fb777b3fc0e
DarkDev <darkdev_brand@riseup.net>
2024-05-07 15:30:03 +0300

    fix margins & paddings

--- src/lib/entities/ui/landing/Price.svelte
+++ src/lib/entities/ui/landing/Price.svelte
@@ -2,11 +2,13 @@
 	export let boardgame;
 </script>
 
+<!-- mb-6 -->
 <div
-	class="mb-6 flex w-full flex-col items-start
+	class=" flex w-full flex-col items-start
        "
 >
-	<div class="mt-2.5 mb-6 w-full">
+<!-- mt-2.5 mb-6  -->
+	<div class="w-full">
 		<!--  justify-between -->
 		<div class=" flex w-full flex-row   text-white">
 			<a href='/download' class="btn-link btn text-neutral-100">Download for free</a>
