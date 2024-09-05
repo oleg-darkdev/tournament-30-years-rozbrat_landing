07f71e8273b7a66cb2250c6c97d535d689e0ba10
DarkDev <darkdev_brand@riseup.net>
2024-09-05 23:00:39 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Gallery.svelte
@@ -0,0 +1,71 @@
+<script>
+
+</script>
+
+<section class="section_content24">
+  <div class="padding-global">
+    <div class="container-large">
+      <div>
+        <div class="content24_component">
+          <div class="margin-bottom margin-xxlarge">
+            <div class="content24_image-wrapper"></div>
+          </div>
+          <div class="w-layout-grid gallery9_component">
+            <div class="w-layout-grid gallery9_row">
+              <a href="#"  class="img-1 gallery9_lightbox-link w-inline-block w-lightbox">
+                <div class="gallery9_image-wrapper">
+                  <img src="" loading="lazy" sizes="90vw"  alt="" class="gallery9_image_large">
+                </div>
+
+              </a>
+
+              <a href="#" class="img-2 gallery9_lightbox-link w-inline-block w-lightbox">
+                <div class="gallery9_image-wrapper">
+                  <img src="" loading="lazy" sizes="90vw"  alt="" class="gallery9_image">
+                </div>
+
+              </a>
+              <a href="#"  class="img-3 gallery9_lightbox-link w-inline-block w-lightbox">
+                <div class="gallery9_image-wrapper">
+                  <img src="" loading="lazy" sizes="90vw"  alt="" class="gallery9_image">
+                </div>
+
+              </a>
+              <a href="#" class="img-4 gallery9_lightbox-link w-inline-block w-lightbox">
+                <div class="gallery9_image-wrapper">
+                  <img src="" loading="lazy" sizes="90vw" alt="" class="gallery9_image">
+                </div>
+
+              </a>
+              <a href="#" class="img-5 gallery9_lightbox-link w-inline-block w-lightbox">
+                <div class="gallery9_image-wrapper">
+                  <img src="" loading="lazy" sizes="90vw"  alt="" class="gallery9_image">
+                </div>
+
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+.img-1 {
+  grid-area: span 2 / span 1 / span 2 / span 1;
+}
+
+@media screen and (max-width: 767px) {
+  .img-1 {
+    grid-area: Area;
+  }
+}
+
+.img-2, .img-3, .img-4, .img-5 {
+  grid-area: span 1 / span 1 / span 1 / span 1;
+}
+
+
+</style>
---
+++ src/lib/widgets/ui/landing/Header.svelte
@@ -0,0 +1,74 @@
+<script>
+  import {navigation} from '$shared';
+</script>
+
+<header  class="navbar16_component w-nav">
+  <div class="navbar16_container">
+    <div class="navbar16_wrapper">
+      <div class="navbar16_menu-button w-nav-button">
+        <div class="menu-icon4">
+          <div class="menu-icon4_wrapper">
+            <div class="menu-icon4_line-top-2"></div>
+            <div class="menu-icon4_line-middle-2">
+              <div class="menu-icon_line-middle-top"></div>
+              <div class="menu-icon_line-middle-base-2"></div>
+            </div>
+            <div class="menu-icon4_line-bottom-2"></div>
+          </div>
+        </div>
+      </div>
+      <nav role="navigation" class="navbar16_menu inherit-color w-nav-menu">
+        <div class="navbar19_menu-wrapper inherit-color">
+          <!-- navigation -->
+          <div class="navbar19_menu-left-top">
+            <a href="/" class="navbar19_link-large w-nav-link">Lorem</a>
+          </div>
+          
+          <div class="navbar19_menu-left-bottom">
+            <div class="text-size-small">© <span class="copyright-year">
+              2024
+
+            </span> Lorem </div>
+            <div class="margin-bottom margin-tiny">
+              <div class="div-block-6">
+                <div class="text-size-small">Lorem impsum is text.</div>
+                <a href="/" target="_blank" class="link-block-2 w-inline-block">
+                  <img src="" loading="lazy" sizes="100vw" alt="logo">
+                </a>
+              </div>
+            </div>
+            <div class="w-layout-grid navbar19_social-list">
+
+              <a href="/" target="_blank" class="social-icons1_link w-inline-block">
+                <div class="icon-embed-xsmall w-embed">
+                  <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22 12.0611C22 6.50451 17.5229 2 12 2C6.47715 2 2 6.50451 2 12.0611C2 17.0828 5.65684 21.2452 10.4375 22V14.9694H7.89844V12.0611H10.4375V9.84452C10.4375 7.32296 11.9305 5.93012 14.2146 5.93012C15.3088 5.93012 16.4531 6.12663 16.4531 6.12663V8.60261H15.1922C13.95 8.60261 13.5625 9.37822 13.5625 10.1739V12.0611H16.3359L15.8926 14.9694H13.5625V22C18.3432 21.2452 22 17.083 22 12.0611Z" fill="CurrentColor"></path>
+                  </svg>
+                </div>
+              </a>
+              <a href="/" target="_blank" class="social-icons1_link w-inline-block">
+                <div class="icon-embed-xsmall w-embed">
+                  <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16 3H8C5.23858 3 3 5.23858 3 8V16C3 18.7614 5.23858 21 8 21H16C18.7614 21 21 18.7614 21 16V8C21 5.23858 18.7614 3 16 3ZM19.25 16C19.2445 17.7926 17.7926 19.2445 16 19.25H8C6.20735 19.2445 4.75549 17.7926 4.75 16V8C4.75549 6.20735 6.20735 4.75549 8 4.75H16C17.7926 4.75549 19.2445 6.20735 19.25 8V16ZM16.75 8.25C17.3023 8.25 17.75 7.80228 17.75 7.25C17.75 6.69772 17.3023 6.25 16.75 6.25C16.1977 6.25 15.75 6.69772 15.75 7.25C15.75 7.80228 16.1977 8.25 16.75 8.25ZM12 7.5C9.51472 7.5 7.5 9.51472 7.5 12C7.5 14.4853 9.51472 16.5 12 16.5C14.4853 16.5 16.5 14.4853 16.5 12C16.5027 10.8057 16.0294 9.65957 15.1849 8.81508C14.3404 7.97059 13.1943 7.49734 12 7.5ZM9.25 12C9.25 13.5188 10.4812 14.75 12 14.75C13.5188 14.75 14.75 13.5188 14.75 12C14.75 10.4812 13.5188 9.25 12 9.25C10.4812 9.25 9.25 10.4812 9.25 12Z" fill="CurrentColor"></path>
+                  </svg>
+                </div>
+              </a>
+              <a href="/" target="_blank" class="social-icons1_link w-inline-block">
+                <div class="icon-embed-xsmall w-embed">
+                  <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M20.5686 4.77345C21.5163 5.02692 22.2555 5.76903 22.5118 6.71673C23.1821 9.42042 23.1385 14.5321 22.5259 17.278C22.2724 18.2257 21.5303 18.965 20.5826 19.2213C17.9071 19.8831 5.92356 19.8015 3.40294 19.2213C2.45524 18.9678 1.71595 18.2257 1.45966 17.278C0.827391 14.7011 0.871044 9.25144 1.44558 6.73081C1.69905 5.78311 2.44116 5.04382 3.38886 4.78753C6.96561 4.0412 19.2956 4.282 20.5686 4.77345ZM9.86682 8.70227L15.6122 11.9974L9.86682 15.2925V8.70227Z" fill="CurrentColor"></path>
+                  </svg>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</header>
+
+
+<style>
+
+</style>
