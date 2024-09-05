67c7551b0538b1931c6509d9845625639dfbb28c
DarkDev <darkdev_brand@riseup.net>
2024-09-05 23:00:57 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/News.svelte
@@ -0,0 +1,79 @@
+<script>
+
+</script>
+
+<section class="">
+<div class="side-menu-wrapper">
+<div class="text-component">
+  <h1 class="hero-text white">Wiadomości</h1>
+</div>
+</div>
+
+<div class="section_portfolio15 inherit-color">
+  <div class="padding-global">
+    <div class="container-large">
+      <div class="padding-section-large">
+        <div class="portfolio15_component">
+          <div class="portfolio15_list-wrapper w-dyn-list">
+            <div role="list" class="portfolio15_list w-dyn-items">
+
+
+              <div role="listitem" class="w-dyn-item">
+                <div class="portfolio15_item">
+                  <a  href="/" class="portfolio15_image-link w-inline-block">
+                    <div class="portfolio15_image-wrapper">
+                      <img src="" loading="lazy" alt="" class="portfolio15_image w-dyn-bind-empty">
+                    </div>
+                  </a>
+                  <div class="portfolio15_title-wrapper">
+                    <div class="portfolio15_content-top">
+                      <div class="portfolio15_tag-list">
+                        <div class="tag-wrapper">
+                          <div class="text-block-basic">Lorem Ipsum </div>
+                        </div>
+                        <div class="tag-wrapper">
+                          <div class="text-block-basic"> Lorem Ipsum </div>
+                        </div>
+                      </div>
+                      <div class="margin-bottom margin-tiny">
+                        <div class="text-block-basic">
+                          March 29, 2024
+                        </div>
+                      </div>
+                      <div class="margin-bottom margin-small">
+                        <h3 class="heading-style-h4">
+                          Lorem Ipsum is simply dummy text
+                        </h3>
+                      </div>
+                      <p  class="text-style-2lines">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                      </p>
+                    </div>
+
+                    <div class="margin-top margin-medium">
+                      <a href="/" class="button is-link is-icon w-inline-block">
+                        <div>Watch</div>
+                        <div class="icon-embed-xxsmall w-embed">
+                          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 3L11 8L6 13" stroke="CurrentColor" stroke-width="1.5"></path>
+                          </svg>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
