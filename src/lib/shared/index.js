ff86b875f61c1ca35ef220260bfd98afd239a156
DarkDev <darkdev_brand@riseup.net>
2024-09-06 15:44:58 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -3,11 +3,11 @@ import faq from './data/faq';
 import agenda from './data/agenda';
 import awards from './data/awards';
 import navigation from './data/navigation';
-// import awards from './data/awards';
+import orgs from './data/orgs';
 
 
 // ui
 // import RegistrationBtn from './ui/RegistrationBtn.svelte';
 
 
-export { faq, agenda, awards, navigation };
+export { faq, agenda, awards, navigation, orgs };
