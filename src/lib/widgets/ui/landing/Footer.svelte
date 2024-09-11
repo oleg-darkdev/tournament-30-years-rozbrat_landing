6feaa787502d263e3917addfb08676c656e149f8
DarkDev <darkdev_brand@riseup.net>
2024-09-11 14:08:44 +0300

    refactoring styles for footer

--- src/lib/widgets/ui/landing/Footer.svelte
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -2,20 +2,29 @@
 
 </script>
 
-<footer class="footer7_component inherit-color">
+<footer class="footer_component inherit-color">
   <div class="padding-global">
     <div>
       <div class="padding-vertical padding-xxlarge">
         <div class="line-divider"></div>
         <div class="padding-top padding-medium">
-          <div class="footer7_bottom-wrapper">
-            <div class="div-block-2">
-              <div class="footer7_credit-text"><span class="text-2xl">❤️ </span>100 lat - Rozbracie.</div>
+          <div class="footer_bottom-wrapper">
+            <div class="flex flex-row justify-between flex-wrap w-full">
+              <div class="footer-text">
+                <div class="text-rich-text w-richtext">
+                  <p>
+                   <span class="text-2xl">❤️ </span> 100 lat - <span class='span-CFPunkAttitude'> <a href="https://www.rozbrat.org/" target="_blank">Rozbracie.</a></span>
+                  </p>
+                </div>
+              </div>
+              <div class="">
+                <div class="text-rich-text w-richtext">
+                  <p>
+                    Development by <span class='span-CFPunkAttitude'> <a href="https://oleg-darkdev.vercel.app/"  target="_blank">oleg.darkdev</a></span>
+                  </p>
+                </div>
+              </div>
             </div>
-            <div class="social-icons1_component">
-              Development by <a href="https://oleg-darkdev.vercel.app/" class='text-[#dc2626]' target="_blank">oleg.darkdev</a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -24,5 +33,32 @@
 
 
 <style>
+.footer_component.inherit-color {
+  color: var(--text-color--text-alternate);
+}
+.footer_bottom-wrapper {
+  justify-content: space-between;
+  display: flex;
+}
+.footer-text {
+  font-family: Courier New, sans-serif;
+  font-size: 0.875rem;
+}
 
+
+@media screen and (max-width: 991px) {
+  .footer_component.inherit-color {
+    color: var(--text-color--text-alternate);
+  }
+}
+@media screen and (max-width: 767px) {
+  .footer_bottom-wrapper {
+    flex-direction: column-reverse;
+    align-items: center;
+    padding-bottom: 1rem;
+  }
+  .footer-text {
+    margin-top: 2rem;
+  }
+}
 </style>
